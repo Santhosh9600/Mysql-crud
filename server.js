cd2247cdb082b93bd3564d774f55dce5e3eacfef
Santhosh9600 <148633650+Santhosh9600@users.noreply.github.com>
2026-04-28 13:17:52 +0530

    commit

--- server.js
+++ server.js
@@ -1,113 +1,109 @@
-const express=require("express")
-const mysql=require("mysql")
-const bodyparse=require("body-parser")
-const cors=require("cors")
+const express = require("express");
+const mysql = require("mysql");
+const bodyparse = require("body-parser");
+const cors = require("cors");
+require("dotenv").config();
 
-const app=express()
+const app = express();
 const port = process.env.PORT || 3002;
 
-//middleware
-
+// middleware
 app.use(cors({
-    origin:"https://mysql-crud-alpha.vercel.app"
-}))
+    origin: "https://mysql-crud-alpha.vercel.app"
+}));
 
-app.use(bodyparse.json())
+app.use(bodyparse.json());
 
-//Mysql
-const db=mysql.createConnection({
-    host:'localhost',
-    user:'root',
-    password:'root',
-    database:'santhoshdb'
-})
+// MySQL
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
 
-db.connect((err)=>{
-    if(!err)
-    {
-        console.log('Db Connected....')
+db.connect((err) => {
+    if (!err) {
+        console.log("Db Connected....");
+    } else {
+        console.log("Db not Connected....", err);
     }
-    else
-    {
-        console.log('Db not Connected....')
-    }
-})
+});
 
 // Home Route
-app.get("/",(req,res)=>{
-    res.send("Server Running : https://mysql-crud-1-cyw5.onrender.com")
-})
+app.get("/", (req, res) => {
+    res.send("Server Running : https://mysql-crud-1-cyw5.onrender.com");
+});
 
-//create table
+// create table
 app.get("/createtable", (req, res) => {
-    let sql = 'CREATE TABLE posts (id INT AUTO_INCREMENT, title VARCHAR(100), body TEXT, PRIMARY KEY(id))';
-    
+    let sql = "CREATE TABLE posts (id INT AUTO_INCREMENT, title VARCHAR(100), body TEXT, PRIMARY KEY(id))";
+
     db.query(sql, (err, result) => {
         if (err) {
             console.log(err);
             res.send("Error creating table");
         } else {
-            console.log('Table created');
-            res.send('Table Created...');
+            res.send("Table Created...");
         }
     });
 });
 
 // Insert post
-app.post('/addpost', (req, res) => {
+app.post("/addpost", (req, res) => {
     let post = { title: req.body.title, body: req.body.body };
-    let sql = 'INSERT INTO posts SET ?';
+    let sql = "INSERT INTO posts SET ?";
 
     db.query(sql, post, (err, result) => {
         if (err) throw err;
-        res.send('Post added...');
+        res.send("Post added...");
     });
 });
 
-//getPosts
-app.get("/getposts",(req,res)=>{
-    let sql="select * from posts";
+// getPosts
+app.get("/getposts", (req, res) => {
+    let sql = "select * from posts";
 
-    db.query(sql,(err,result)=>{
-        if(err) throw err;
-        res.send(result)
-    })
-})
+    db.query(sql, (err, result) => {
+        if (err) throw err;
+        res.send(result);
+    });
+});
 
-//get post by id
-app.get('/getpost/:id',(req,res)=>{
-    let sql=`select * from posts where id=${req.params.id}`;
+// get post by id
+app.get("/getpost/:id", (req, res) => {
+    let sql = "select * from posts where id=?";
 
-    db.query(sql,(err,result)=>{
-        if(err) throw err;
+    db.query(sql, [req.params.id], (err, result) => {
+        if (err) throw err;
         res.json(result);
-    })
-})
+    });
+});
 
-//update post
-app.put('/updatepost/:id',(req,res)=>{
-    const {id}=req.params;
-    const {title,body}=req.body;
+// update post
+app.put("/updatepost/:id", (req, res) => {
+    const { id } = req.params;
+    const { title, body } = req.body;
 
-    const sql=`update posts set title=?,body=? where id=?`;
+    const sql = "update posts set title=?,body=? where id=?";
 
-    db.query(sql,[title,body,id],(err,result)=>{
-        if(err) throw err;
-        res.send('post updated..')
-    })
-})
+    db.query(sql, [title, body, id], (err, result) => {
+        if (err) throw err;
+        res.send("post updated..");
+    });
+});
 
-//delete post
-app.delete('/deletepost/:id',(req,res)=>{
-    const {id}=req.params;
-    const sql=`delete from posts where id=?`;
+// delete post
+app.delete("/deletepost/:id", (req, res) => {
+    const { id } = req.params;
+    const sql = "delete from posts where id=?";
 
-    db.query(sql,[id],(err,result)=>{
-        if(err) throw err;
-        res.send('posted deleted')
-    })
-})
+    db.query(sql, [id], (err, result) => {
+        if (err) throw err;
+        res.send("posted deleted");
+    });
+});
 
 app.listen(port, () => {
-    console.log("server is Running.....");
+    console.log(`server is Running on ${port}`);
 });

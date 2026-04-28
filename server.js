35db186abde32a34358465cb53e08193e3882a1b
Santhosh9600 <148633650+Santhosh9600@users.noreply.github.com>
2026-04-28 12:14:32 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,99 @@
+const express=require("express")
+const mysql=require("mysql")
+const bodyparse=require("body-parser")
+const cors=require("cors")
+
+const app=express()
+const port=3002;
+
+//middleware
+
+app.use(cors())
+app.use(bodyparse.json())
+
+//Mysql
+const db=mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'root',
+    database:'santhoshdb'
+})
+db.connect((err)=>{
+    if(!err)
+    {
+        console.log('Db Connected....')
+    }
+      else
+    {
+        console.log('Db not Connected....')
+    }
+})
+
+//create table
+app.get("/createtable", (req, res) => {
+    let sql = 'CREATE TABLE posts (id INT AUTO_INCREMENT, title VARCHAR(100), body TEXT, PRIMARY KEY(id))';
+    
+    db.query(sql, (err, result) => {
+        if (err) {
+            console.log(err);
+            res.send("Error creating table");
+        } else {
+            console.log('Table created');
+            res.send('Table Created...');
+        }
+    });
+});
+
+
+// Insert post
+app.post('/addpost', (req, res) => {
+    let post = { title: req.body.title, body: req.body.body };
+    let sql = 'INSERT INTO posts SET ?';
+    db.query(sql, post, (err, result) => {
+        if (err) throw err;
+        res.send('Post added...');
+    });
+});
+
+//getPosts
+app.get("/getposts",(req,res)=>{
+    let sql="select * from posts";
+    db.query(sql,(err,result)=>{
+        if(err)
+            throw err;
+        res.send(result)
+    })
+})
+//get post by id
+
+app.get('/getpost/:id',(req,res)=>{
+    let sql=`select * from posts where id=${req.params.id}`;
+    db.query(sql,(err,result)=>{
+        if(err)throw err;
+        res.json(result);
+    })
+})
+//update post
+app.put('/updatepost/:id',(req,res)=>{
+    const {id}=req.params;
+    const {title,body}=req.body;
+    const sql=`update posts set title=?,body=? where id=?`;
+    db.query(sql,[title,body,id],(err,result)=>{
+        if(err)throw err;
+        res.send('post updated..')
+    })
+})
+
+//delete post
+app.delete('/deletepost/:id',(req,res)=>{
+    const {id}=req.params;
+    const sql=`delete from posts where id=?`;
+    db.query(sql,[id],(err,result)=>{
+          if(err)throw err;
+          res.send('posted deleted')
+    })
+})
+
+app.listen(port,()=>{
+    console.log("server is Running.....")
+})
